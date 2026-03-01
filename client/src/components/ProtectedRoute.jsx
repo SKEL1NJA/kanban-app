b146dfb27becd4c58e48f3fd301688f174a092a9
SKEL1NJA <khannauday55@gmail.com>
2026-03-01 22:48:10 +0530

    Frontend Step 6: Protected routes added

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+export default function ProtectedRoute({ children }) {
+
+  const { user } = useContext(AuthContext);
+
+  // ❌ Not logged in
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
+
+  // ✅ Logged in
+  return children;
+}
