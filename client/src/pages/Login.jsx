ed166f70eaca9ea6e8a373807326cb10af05adef
SKEL1NJA <khannauday55@gmail.com>
2026-03-01 22:44:16 +0530

    Frontend Step 5.3: Login integration completed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,75 @@
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../api/axios";
+import { AuthContext } from "../context/AuthContext";
+
 export default function Login() {
+
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post("/auth/login", form);
+
+      // ✅ save user + token
+      login(res.data);
+
+      // ✅ redirect
+      navigate("/dashboard");
+
+    } catch (err) {
+      alert(err.response?.data?.message || "Login failed");
+    }
+  };
+
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-3xl">Login Page</h1>
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      
+      <form
+        onSubmit={handleSubmit}
+        className="bg-gray-800 p-8 rounded w-96 space-y-4"
+      >
+        <h2 className="text-white text-2xl text-center">
+          Login
+        </h2>
+
+        <input
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          className="w-full p-2 rounded"
+        />
+
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+          className="w-full p-2 rounded"
+        />
+
+        <button
+          className="w-full bg-blue-500 text-white p-2 rounded"
+        >
+          Login
+        </button>
+      </form>
+
     </div>
   );
 }
