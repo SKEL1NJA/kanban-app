18061ecb078b6b996bf318da31c5eafc26aa8ecd
SKEL1NJA <khannauday55@gmail.com>
2026-03-01 19:26:06 +0530

    Frontend Step 2: Tailwind Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Kanban App 🚀</h1>
+    <div className="h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind Working 🚀
+      </h1>
     </div>
   );
 }
