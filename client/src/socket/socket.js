52342344e308f55e6e4b49ae6d73520cac4996ef
SKEL1NJA <khannauday55@gmail.com>
2026-03-05 21:05:37 +0530

    Step 24: Production Socket.IO configuration

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL =
   import.meta.env.VITE_SOCKET_URL ||
-  "http://localhost:5000";
+  "https://kanban-api-6djm.onrender.com";
 
 const socket = io(SOCKET_URL, {
   transports: ["websocket"],
